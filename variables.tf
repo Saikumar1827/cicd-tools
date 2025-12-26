e12b18e6409c70b6a4a18fb11ea757fc333dc410
sai goud <saikumarmogulla40@gmail.com>
2025-12-26 22:53:58 +0530

    jenkins

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "saidaws86s.fun"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z0506429205Z4ZA4IXHNC"
+  description = "description"
+}
+
+variable "sonar" {
+  default = false
+}
